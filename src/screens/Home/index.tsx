d7bc0db3d4a90ab35adbf9e7ab704e13bae120b6
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2021-06-14 16:01:21 -0300

    dev: Add home page

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import Car from '../../components/Car';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  CarList,
+} from './styles';
+
+export default function Home(){
+  const carData = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120
+    },
+    thumbnail: 'https://production.autoforce.com/uploads/version/profile_image/3188/model_main_comprar-tiptronic_87272c1ff1.png',
+  };
+
+  return (
+    <Container>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+            />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1,2,3,4,5,6,7]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
+
+    </Container>
+  );
+}
