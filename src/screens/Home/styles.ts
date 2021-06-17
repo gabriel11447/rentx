cb0bc26a449f844b67b601ae30143ef6b0ac5d9c
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2021-06-17 16:04:55 -0300

    dev: Add floating button animation

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -36,15 +36,3 @@ export const CarList = styled(FlatList as new () => FlatList<CarDTO>)
   },
   showsVerticalScrollIndicator: false
 })``;
-
-export const MyCarsButton = styled(RectButton)`
-  width: 60px;
-  height: 60px;
-  background-color: ${({ theme }) => theme.colors.main};
-  justify-content: center;
-  align-items: center;
-  border-radius: 30px;
-  position: absolute;
-  bottom: 13px;
-  right: 22px;
-`;
