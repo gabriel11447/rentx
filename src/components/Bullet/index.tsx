590e977d6995fa296d92147c3cf6eaca95259d42
Gabriel de Carvalho <gabriel.de.carvalho_@hotmail.com>
2021-06-21 10:01:22 -0300

    dev: Add bullet component

---
+++ src/components/Bullet/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+  Container,
+} from './styles';
+
+interface Props {
+  active?: boolean;
+}
+
+export default function Bullet({ active = false }: Props){
+  return (
+    <Container active={active} />
+  );
+}
